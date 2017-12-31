3c3a1a25a5aac70bee80f81fb23a7aa0117cdc40
Luke <lchenhw@gmail.com>
2017-12-30 23:47:30 -0600

    added style to game_gui.py

--- game_gui.py
+++ game_gui.py
@@ -26,11 +26,12 @@ class Game_Gui:
 		self.score_bt_txt = tk.StringVar()
 		self.new_game_bt_txt = tk.StringVar()
 
+		# actual labels for storing info
 		self.pris_lab = None
 		self.turn_lab = None
 		self.score_lab = None
 
-		# stores references to each label
+		# stores references to each label for new update
 		self.label_grid = np.ndarray(shape=(size,size), dtype=object)
 
 		self._set_gui_values()
@@ -74,32 +75,31 @@ class Game_Gui:
 			command = self._new_game_on_click)
 		self.bt.pack(side = tk.RIGHT, fill = tk.X)
 
-# These are the FINAL functionalities!!!
-
-	# double callback to change text of button while running for new game
-	def new_game_callback(self):
+	# second callback to change text of button while running for new game
+	def _new_game_callback(self):
 		self.my_game = bd.Game(self.komi, (np.zeros(shape = (self.size, self.size))).astype('int'))
 		self.turn = 1
 		self.the_turn.set("Turn: Black")
-		self.gui_update()
+		self._gui_update()
 		self.new_game_bt_txt.set("New Game")
 		self.turn = -1
 
+	# stored action for new game on click
 	def _new_game_on_click(self):
 		self.new_game_bt_txt.set("Clearing Board...")
-		self.root.after(20, self.new_game_callback)
+		self.root.after(20, self._new_game_callback)
 
+	# stored action for playing passing on click
 	def _pass_on_click(self):
-		print("we are passing")
-
 		move = (0, 0, 0) # final zero means pass
 		successful_move = self.my_game.update(move) 
 
 		if successful_move:
-			self.gui_update()
+			self._gui_update()
 			self.turn = bd.flip(self.turn) # after turn ends, you flip
 
-	def scoring_callback(self):
+	# second callback to change text of button while scoring
+	def _scoring_callback(self):
 		score_obj = sc.Scoring(self.my_game)
 		score_dict = score_obj.score_it()
 		score_sts = "Black Score: " + str(score_dict[-1]) + "   " \
@@ -107,9 +107,10 @@ class Game_Gui:
 		self.my_score.set(score_sts)
 		self.score_bt_txt.set("Score It")
 
+	# stored action for scoring on click
 	def _score_on_click(self):
 		self.score_bt_txt.set("Scoring...")
-		self.root.after(20, self.scoring_callback)
+		self.root.after(20, self._scoring_callback)
 
 	# All of these get new text values of the status bar
 	def _update_status(self):
@@ -129,7 +130,7 @@ class Game_Gui:
 		self.the_turn.set(turn_sts)
 		self.prisoners.set(prsn_sts)
 
-
+	# initially creates all the labels for the board
 	def create_board(self):
 		my_board = self.my_game.board
 		im_blck = tk.PhotoImage(file='~/Go/img/black.gif')
@@ -156,6 +157,7 @@ class Game_Gui:
 				lab.grid(row=i,column=j, padx = (0, 0), pady = (0,0))
 				lab.bind('<Button-1>',lambda e,i=i,j=j: self._on_click(i,j,e))
 			
+	# updates the images in the cell
 	def alter_board_cell(self, my_board, i, j):
 		
 		im_blck = tk.PhotoImage(file='~/Go/img/black.gif')
@@ -187,21 +189,22 @@ class Game_Gui:
 		except:
 			return False
 
-	def update_board(self):
+	# updates board. called when something happens to board
+	def _update_board(self):
 		# get current status of board from my_game instance
 		my_board = self.my_game.board
 
 		for i in range(self.size):
 			for j in range(self.size):
-				# try only updating when board changes
+				# only updates cell if changed. Otherwise takes too long
 				if self._board_changed(my_board, i, j) or len(self.my_game.board_hist) < 2:
 					self.alter_board_cell(my_board,i, j)
 	
 
 	# turns the board game into the right one
-	def gui_update(self):
+	def _gui_update(self):
 		print("starting updating the board...")
-		self.update_board()
+		self._update_board()
 		print("starting updating the status...")
 		self._update_status()
 		print("done computing updates...")
@@ -211,14 +214,13 @@ class Game_Gui:
 		print(i, j, self.turn)
 		move = i, j, self.turn
 
-		# all logic on board updating is contained here. 
+		# all logic on board updating is contained here
 		successful_move = self.my_game.update(move) 
 
 		# takes the game board and then updates it.
 		if successful_move:
-			self.gui_update()
+			self._gui_update()
 			self.turn = bd.flip(self.turn) # after turn ends, you flip
-
 
 my_game_gui = Game_Gui(20, 5.5)
 my_game_gui.create_board()
--- scoring.py
+++ scoring.py
@@ -14,7 +14,8 @@ class Scoring:
 		# we do not want to go over blanks twice
 		self.seen_board = np.zeros(shape = (self.board_sz, self.board_sz))
 
-	def add_score(self, blank_ls, stone_ls):
+	# add up the score given list of blanks for a particular 'patch'
+	def _add_score(self, blank_ls, stone_ls):
 		# must decide if 'Dame' for scoring  
 		has_white = False
 		has_black = False
@@ -25,8 +26,6 @@ class Scoring:
 				has_black = True
 			elif self.board[coord[0], coord[1]] == 1:
 				has_white = True
-			else:
-				pass
 		
 		# exclusive or to check for 'dame'
 		if has_black ^ has_white:
@@ -41,12 +40,14 @@ class Scoring:
 			else:
 				pass
 
-	def add_to_seen(self, blank_ls):
+	# keep track of parts of grid you have seen before
+	def _add_to_seen(self, blank_ls):
 		for coord in blank_ls:
 			self.seen_board[coord[0], coord[1]] = 1 # set it equal to 1
 
 	# recursively counts for score 
-	def flood_fill_cnt(self, start, blank_ls, stone_ls):
+	# main logic for counting score
+	def _flood_fill_cnt(self, start, blank_ls, stone_ls):
 		row, col  = start[0], start[1]
 
 		if not bd.in_board(start, self.board_sz) or start in blank_ls:
@@ -59,27 +60,28 @@ class Scoring:
 		right = (row, col + 1)
 		directions = [up, down, left, right]
 
+		# LUCAS PLEASE REVIEW THIS BLOCK
 		# We need fast performance - need to check if valid BEFORE we recurse
 		if self.board[row, col] == 0: # it's a blank. keep counting score.
 			blank_ls.append((row, col))
 
 			if bd.in_board(up, self.board_sz) and up not in blank_ls:
-				blank_ls1, stone_ls1 = self.flood_fill_cnt(up, blank_ls, stone_ls)
+				blank_ls1, stone_ls1 = self._flood_fill_cnt(up, blank_ls, stone_ls)
 			else: 
 				blank_ls1, stone_ls1 = blank_ls, stone_ls
 
 			if bd.in_board(down, self.board_sz) and down not in blank_ls:
-				blank_ls2, stone_ls2 = self.flood_fill_cnt(down, blank_ls, stone_ls)
+				blank_ls2, stone_ls2 = self._flood_fill_cnt(down, blank_ls, stone_ls)
 			else: 
 				blank_ls2, stone_ls2 = blank_ls, stone_ls
 
 			if bd.in_board(left, self.board_sz) and left not in blank_ls:
-				blank_ls3, stone_ls3 = self.flood_fill_cnt(left, blank_ls, stone_ls)
+				blank_ls3, stone_ls3 = self._flood_fill_cnt(left, blank_ls, stone_ls)
 			else: 
 				blank_ls3, stone_ls3 = blank_ls, stone_ls
 
 			if bd.in_board(right, self.board_sz) and right not in blank_ls:
-				blank_ls4, stone_ls4 = self.flood_fill_cnt(right, blank_ls, stone_ls)
+				blank_ls4, stone_ls4 = self._flood_fill_cnt(right, blank_ls, stone_ls)
 			else: 
 				blank_ls4, stone_ls4 = blank_ls, stone_ls
 
@@ -93,26 +95,29 @@ class Scoring:
 		blank_ls = blank_ls1 + blank_ls2 + blank_ls3 + blank_ls4
 		stone_ls = stone_ls1 + stone_ls2 + stone_ls3 + stone_ls4
 	
-		#stone_ls = (all_stone_ls[0] + all_stone_ls[1]) + (all_stone_ls[2] + all_stone_ls[3]) 
-
 		return blank_ls, stone_ls
 
+	# score each of the positions by counting territories surrounded by colors
+	# returns a final score
 	def score_it(self):
 
+		# find all 'patches' of empty board spaces surrounded by a color
 		for i in range(self.board_sz):
 			for j in range(self.board_sz):
+				
+				# make sure you don't count the same patch twice
 				if self.seen_board[i, j] == 0:
-					blank_ls, stone_ls = self.flood_fill_cnt((i, j), [], [])
-					# remove duplicates, then turn to set again
+					blank_ls, stone_ls = self._flood_fill_cnt((i, j), [], [])
+
+					# remove duplicates, then add the score up using 
 					if len(blank_ls) > 0:
 						blank_ls = list(set(blank_ls))
-						self.add_to_seen(blank_ls)
-						self.add_score(blank_ls, stone_ls)
+						self._add_to_seen(blank_ls)
+						self._add_score(blank_ls, stone_ls)
 
 		# count opposite color which is captured, and add to score
 		black_score = self.score[-1] + self.prisoners[1]
 		white_score = self.score[1] + self.prisoners[-1]
-
 
 		final_score = {-1: black_score, 1: white_score + self.komi} 
 		return final_score
